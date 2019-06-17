f2570886f8a6e33d6f40d941ffb9e8216e006748
weintraubje <32884134+weintraubje@users.noreply.github.com>
2019-06-17 11:44:41 -0400

    Add files via upload
    Updated the file so now categorical data -> 0/1 data in multiple columns

--- CleanData.R
+++ CleanData.R
@@ -15,10 +15,11 @@ baseData$date_time <- paste(baseData$date_time, ":00", sep = "")
 baseData$date_time <- as.POSIXct(baseData$date_time, format = "%m/%d/%Y %H:%M:%S")
 baseData$hour <- hour(baseData$date_time)
 baseData$month <- month(baseData$date_time)
-baseData$weekday <- wday(baseData$date_time)
-baseData$weekday[baseData$weekday<6] <- 0 # Designates weekday
-baseData$weekday[baseData$weekday>5] <- 1 # Designates weekend
+baseData$weekend <- wday(baseData$date_time)
+baseData$weekend[baseData$weekday<6] <- 0 # Designates weekday
+baseData$weekend[baseData$weekday>5] <- 1 # Designates weekend
 baseData$date <- date(baseData$date_time)
+baseData$year <- year(baseData$date_time) # Not used in calculations, just in segmenting data
 
 # Holidays fix and visual inspection for grouping
 # 0 = No holiday
@@ -29,13 +30,13 @@ for (i in 1:length(hDates)) {
   temp <- baseData$holiday[baseData$date_time == hDates[i]]
   baseData$holiday[baseData$date == hDates[i]] <- temp[1]
 }
-#holidayData <- filter(baseData, holiday != "None")
-#ggplot(holidayData, aes(x = traffic_volume, y = holiday)) + geom_point()
+holidayData <- filter(baseData, holiday != "None")
+ggplot(holidayData, aes(x = traffic_volume, y = holiday)) + geom_point()
 baseData$holiday[baseData$holiday == "None"] <- "0"
 baseData$holiday[baseData$holiday == "Washingtons Birthday" | baseData$holiday == "Veterans Day" |
-                   baseData$holiday == "State Fair" | baseData$holiday == "Columbus Day"] <- "1"
-baseData$holiday[baseData$holiday != "0" & baseData$holiday != "1"] <- "2"
-#holidayData <- filter(baseData, holiday != "None")
+                   baseData$holiday == "State Fair" | baseData$holiday == "Columbus Day"] <- "0"
+baseData$holiday[baseData$holiday != "0" & baseData$holiday != "1"] <- "1"
+holidayData <- filter(baseData, holiday != "None")
 #ggplot(holidayData, aes(x = traffic_volume, y = holiday)) + geom_point()
 rm(holidayData, hDates, i, temp)
 
@@ -45,6 +46,23 @@ baseData$temp  <- as.numeric(baseData$temp)
 baseData$rain_1h  <- as.numeric(baseData$rain_1h)
 baseData$snow_1h  <- as.numeric(baseData$snow_1h)
 baseData$hour  <- as.numeric(baseData$hour)
+baseData$year  <- as.numeric(baseData$year)
+
+# Weather Columns
+baseData$RAIN <- baseData$SNOW <- baseData$THUNDERSTORM <- baseData$MIST <- 0
+baseData$DRIZZLE <- baseData$FOG <- baseData$HAZE <- baseData$SQUALL <- 0
+baseData$SMOKE <- baseData$CLOUDS <- baseData$CLEAR <- 0
+baseData$RAIN[baseData$weather_main == "RAIN"] <- 1
+baseData$SNOW[baseData$weather_main == "SNOW"] <- 1
+baseData$THUNDERSTORM[baseData$weather_main == "THUNDERSTORM"] <- 1
+baseData$MIST[baseData$weather_main == "MIST"] <- 1
+baseData$DRIZZLE[baseData$weather_main == "DRIZZLE"] <- 1
+baseData$FOG[baseData$weather_main == "FOG"] <- 1
+baseData$HAZE[baseData$weather_main == "HAZE"] <- 1
+baseData$SQUALL[baseData$weather_main == "SQUALL"] <- 1
+baseData$SMOKE[baseData$weather_main == "SMOKE"] <- 1
+baseData$CLOUDS[baseData$weather_main == "CLOUDS"] <- 1
+baseData$CLEAR[baseData$weather_main == "CLEAR"] <- 1
 
 # Normalize Values
 normalizedData <- baseData
