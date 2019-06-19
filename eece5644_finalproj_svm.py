69da9dbe36afecf12c962930d10ed56ed4e0a8e6
Nicholas Fresneda <nicholasfresneda@gmail.com>
2019-06-18 21:36:22 -0400

    final revisions

--- eece5644_finalproj_svm.py
+++ eece5644_finalproj_svm.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
+from sklearn.svm import SVC, SVR
 import math
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
@@ -24,20 +24,17 @@ def get_score(pred, ytest, error):
     return count / len(y_pred)
 
 
+# get data from csv
 traffic_column = 8
 input_data = pd.read_csv("CleanedData.csv")
-# Preview the first 5 lines of the loaded data
-datasize = 2500
+datasize = 1000
 
-preX = input_data.drop('traffic_volume', axis=1).drop('date', axis=1).drop('date_time', axis=1).drop('weather_main',
-                                                                                                     axis=1).drop(
-    'weather_description', axis=1)
+# split data into training and test sets
+preX = input_data.drop('traffic_volume', axis=1).drop('date', axis=1).drop('date_time', axis=1).drop('weather_main',  axis=1).drop('weather_description', axis=1)
 X = preX.iloc[0:datasize]
-
 preY = input_data['traffic_volume']
 Y = preY.iloc[0:datasize]
-
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.80)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.30)
 
 # run k fold cross validation on C value and svc kernel functions 'linear' and 'rbf'
 K = 10
@@ -46,8 +43,9 @@ k_rankings = np.zeros((K, 3))
 error_range = 0.10
 rbf = 0
 linear = 1
+scores = np.zeros((len(Clist), 2))
 for k in range(0, K):
-    x_k_train, x_k_test, y_k_train, y_k_test = train_test_split(X_train, y_train, test_size=0.80)
+    x_k_train, x_k_test, y_k_train, y_k_test = train_test_split(X_train, y_train, test_size=0.30)
     max = -math.inf
     bestfun = 0
     bestC = 0
@@ -61,6 +59,7 @@ for k in range(0, K):
             k_svclassifier.fit(x_k_train, y_k_train)
             y_pred = k_svclassifier.predict(x_k_test)
             score = get_score(y_pred, y_k_test, error_range)
+            scores[c, fun] = score
             if score > max:
                 max = score
                 bestfun = fun
@@ -70,6 +69,12 @@ for k in range(0, K):
     k_rankings[k, 1] = bestfun
     k_rankings[k, 2] = bestC
 
+
+# graph k fold validation
+plt.figure(1)
+plt.plot(Clist, scores[:, 1], 'bo', label = 'linear')
+plt.plot(Clist, scores[:, 0], 'ro', label = 'rbf')
+plt.legend(loc='upper left')
 # find best C and function
 best_k = np.argmax(k_rankings[:, 0])
 if k_rankings[best_k, 1] == rbf:
@@ -77,9 +82,10 @@ if k_rankings[best_k, 1] == rbf:
 else:
     svclassifier = SVC(kernel='linear', C=k_rankings[best_k, 2], gamma='auto')
 
+# fit classifier to training data and test
 svclassifier.fit(X_train, y_train)
 y_pred = svclassifier.predict(X_test)
-print("Predictions correct within 10% margin" + str(get_score(y_pred, y_test, error_range)))
+print("Predictions correct within 10% margin " + str(get_score(y_pred, y_test, error_range)))
 print("best function " + str(k_rankings[best_k, 1]))
 print("best C " + str(k_rankings[best_k, 2]))
 print("mean squared error " + str(mean_squared_error(y_test, y_pred)))
@@ -90,4 +96,16 @@ new_df.insert(len(new_df.columns), "predictions", y_pred)
 new_df.to_csv("results.csv")
 
 # plot data
-plt.plot(y_test, y_pred, 'o')   
+plt.figure(2)
+plt.plot(y_test, y_pred, 'o', alpha=0.3)
+plt.plot([0, 7000], [0, 7000], color="orange")
+plt.show()
+
+percents = np.zeros(100)
+for i in range(0, 100):
+    percents[i] = get_score(y_pred, y_test, i * 0.01)
+
+x_axis = np.linspace(0, 100, 100)
+plt.figure(3)
+plt.plot(x_axis, percents)
+plt.show()
